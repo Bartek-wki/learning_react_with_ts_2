59b67a5695fe57c3f3fe69d25ffaeb4ca34a1a60
unknown <b.wilki0109@gmail.com>
2025-11-15 09:22:59 +0100

    make timers runing

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,45 @@
 import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useTimersContext, type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Timer({name, duration}: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000)
+  const interval = useRef<number | null>(null);
+  const { isRunning } = useTimersContext()
+
+  useEffect(() => {
+    let timer: number;
+
+    if(isRunning) {
+      timer = setInterval(function(){
+        setRemainingTime(prevTime => {
+          if(prevTime <= 0) {
+            return prevTime;
+          }
+
+          return prevTime - 50
+        })
+      }, 50)
+
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current)
+    }
+
+    return () => clearInterval(timer)
+  }, [isRunning])
+
+  if(remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current)
+  }
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <progress value={remainingTime} max={duration * 1000} />
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
